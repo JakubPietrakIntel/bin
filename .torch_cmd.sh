d5203ad0bb35607d1216d70bb67283df45e23345
Jakub Pietrak <jakub.pietrak@intel.com>
2022-09-22 14:12:57 +0200

    rm touch .log

--- .torch_cmd.sh
+++ .torch_cmd.sh
@@ -88,7 +88,6 @@ function pttest() {
 }
 
 function ptpip() {
-	touch installation.log
 	if [[ $1 == "pytorch" ]]; then
 		REL_WITH_DEB_INFO=false USE_CUDA=false python -m pip install --verbose -e . $2 >> ../install_${1}.log
 	else
