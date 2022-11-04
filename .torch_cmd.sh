b747efe1fd6e22f9f1c26ffaf86691d0572f2f34
Jakub Pietrak <jakub.pietrak@intel.com>
2022-11-04 16:38:10 +0100

    install psutil

--- .torch_cmd.sh
+++ .torch_cmd.sh
@@ -159,6 +159,7 @@ function ptconda() {
 	conda install -y astunparse numpy ninja pyyaml cmake cffi typing_extensions future six requests dataclasses pandas pytest
 	python -m pip install --upgrade setuptools
 	python -m pip install ogb --no-deps
+	python -m pip install psutil
 }
 
 function ptgitlog() {
